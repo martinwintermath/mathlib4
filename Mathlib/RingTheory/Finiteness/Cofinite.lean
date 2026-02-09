b73a697e4129a8f73715dfea38009f96e878cb92
Martin Winter <martin.winter.math@gmail.com>
2026-02-09 21:09:28 +0100

    minor formatting

--- Mathlib/RingTheory/Finiteness/Cofinite.lean
+++ Mathlib/RingTheory/Finiteness/Cofinite.lean
@@ -72,7 +72,7 @@ theorem range_fg_iff_ker_cofg {f : M →ₗ[R] N} : (range f).FG ↔ (ker f).CoF
 
 /-- The kernel of a linear map into a noetherian module is CoFG. -/
 protected theorem CoFG.ker [IsNoetherian R N] (f : M →ₗ[R] N) : (ker f).CoFG :=
-    range_fg_iff_ker_cofg.mp <| IsNoetherian.noetherian _
+  range_fg_iff_ker_cofg.mp <| IsNoetherian.noetherian _
 
 end LinearMap
 
